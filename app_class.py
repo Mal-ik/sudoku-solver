53709935582fe68bdc38b71c19ab93c330416cf5
Malik <46856853+Mal-ik@users.noreply.github.com>
2020-08-18 22:18:10 -0400

    Implimented GUI

--- app_class.py
+++ app_class.py
@@ -1,5 +1,7 @@
 import sys
 import pygame
+import requests  # gets html
+from bs4 import BeautifulSoup  # allows us to manipulate html
 from settings import *
 from buttonClass import *
 from solver import *
@@ -11,7 +13,6 @@ class App:
         self.window = pygame.display.set_mode(
             (WIDTH, HEIGHT))  # creates display window
         self.running = True  # checks whether game is running
-        self.grid = finishedBoard
         self.selected = None  # when something selected
         self.mousePos = None  # mouse pos
         self.state = "playing"  # determines state of board
@@ -19,11 +20,9 @@ class App:
         self.finished = False
         self.incorrectCells = []
         self.playingButtons = []  # stores buttons for the play state
-        self.menuButtons = []
-        self.endButtons = []
         self.lockedCells = []
         self.font = pygame.font.SysFont('Arial', cellSize // 2)
-        self.load()  # to keep init function neater
+        self.getPuzzle("3")
 
     # runs game functions while its running
     def run(self):
@@ -48,8 +47,10 @@ class App:
                 if selected:
                     self.selected = selected
                 else:
-                    print('not on grid')
                     self.selected = None
+                    for button in self.playingButtons:
+                        if button.highlighted:
+                            button.click()
 
             # User types
             if event.type == pygame.KEYDOWN:
@@ -72,7 +73,7 @@ class App:
                 self.finished = True
                 self.checkAllCells()
                 if not self.incorrectCells:
-                    print('Congratulations')
+                    self.finished = True
 
     # draws board in play state
     def playing_draw(self):
@@ -111,6 +112,30 @@ class App:
                         self.incorrectCells.append([ridx, cidx])
 
 ##### HELPER FUNCTIONS #####
+    def getPuzzle(self, difficulty):
+        # difficulty must be passed in as string ("1"-"4")
+        html_doc = requests.get(
+            f"https://nine.websudoku.com/?level={difficulty}").content
+        soup = BeautifulSoup(html_doc, features="html.parser")
+        ids = ['f00', 'f01', 'f02', 'f03', 'f04', 'f05', 'f06', 'f07', 'f08', 'f10', 'f11',
+               'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f20', 'f21', 'f22', 'f23',
+               'f24', 'f25', 'f26', 'f27', 'f28', 'f30', 'f31', 'f32', 'f33', 'f34', 'f35',
+               'f36', 'f37', 'f38', 'f40', 'f41', 'f42', 'f43', 'f44', 'f45', 'f46', 'f47',
+               'f48', 'f50', 'f51', 'f52', 'f53', 'f54', 'f55', 'f56', 'f57', 'f58', 'f60',
+               'f61', 'f62', 'f63', 'f64', 'f65', 'f66', 'f67', 'f68', 'f70', 'f71', 'f72',
+               'f73', 'f74', 'f75', 'f76', 'f77', 'f78', 'f80', 'f81', 'f82', 'f83', 'f84',
+               'f85', 'f86', 'f87', 'f88']
+        data = []
+        for cid in ids:
+            data.append(soup.find('input', id=cid))
+        board = [[0 for x in range(9)] for x in range(9)]
+        for index, cell in enumerate(data):
+            try:
+                board[index//9][index % 9] = int(cell['value'])
+            except:
+                pass
+        self.grid = board
+        self.load()
 
     def drawSelection(self, window, pos):
         pygame.draw.rect(
@@ -155,7 +180,16 @@ class App:
 
     # creates all buttons
     def loadButtons(self):
-        self.playingButtons.append(Button(20, 40, 100, 40))
+        self.playingButtons.append(Button(
+            20, 40, WIDTH//7, 40, function=self.checkAllCells, colour=(27, 142, 207), text="Check"))
+        self.playingButtons.append(Button(
+            140, 40, WIDTH//7, 40, function=self.getPuzzle, params="1",  colour=(117, 172, 112), text="Easy"))
+        self.playingButtons.append(Button(
+            260, 40, WIDTH//7, 40, function=self.getPuzzle, params="2",  colour=(204, 197, 110), text="Medium"))
+        self.playingButtons.append(Button(
+            380, 40, WIDTH//7, 40, function=self.getPuzzle, params="3",  colour=(199, 129, 48), text="Hard"))
+        self.playingButtons.append(Button(
+            500, 40, WIDTH//7, 40, function=self.getPuzzle, params="4",  colour=(207, 68, 67), text="Evil"))
 
     def textToScreen(self, window, text, pos, color=BLACK):
         font = self.font.render(text, False, color)
@@ -166,7 +200,11 @@ class App:
         window.blit(font, pos)
 
     def load(self):
+        self.playingButtons = []
         self.loadButtons()
+        self.lockedCells = []
+        self.incorrectCells = []
+        self.finshed = False
 
         # setting locked cells from original board
         for ridx, row in enumerate(self.grid):
